c53a08972080f81a84f5732c581dcddf5f8636f5
JHart96 <jordan.da.hart@gmail.com>
2024-01-24 12:36:22 +0000

    Added data listing.

--- causadb/cli/data.py
+++ causadb/cli/data.py
@@ -1,4 +1,10 @@
 import typer
+import requests
+from causadb.cli.utils import load_config, show_table, CAUSADB_API_URL
+import json
+
+from rich.console import Console
+from rich.table import Table
 
 app = typer.Typer()
 
@@ -8,7 +14,17 @@ def list():
     """
     List linked datasources.
     """
-    typer.echo("Listing linked datasources")
+    config = load_config()
+    token_id = config["default"]["token_id"]
+    token_secret = config["default"]["token_secret"]
+
+    headers = {"token": token_secret}
+
+    data = requests.get(
+        f"{CAUSADB_API_URL}/cli/list-data", headers=headers
+    ).json()
+
+    show_table(data["data"], columns=["id", "name", "type"])
 
 
 @app.command()
