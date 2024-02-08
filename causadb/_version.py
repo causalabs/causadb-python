08c5410e01be1a9ad8a6f8f50e59ffd773f9cbcd
JHart96 <jordan.da.hart@gmail.com>
2024-02-08 17:00:50 +0000

    Add PyPi deployment.

--- causadb/_version.py
+++ causadb/_version.py
@@ -1,4 +1,4 @@
-# Reset to -1 for new minor/major version
-major = 0
-minor = 1
-patch = 7  # 1dca4be7d0990856ab43114883c3729b0c901e47
+# Reset patch to -1 for new minor/major version
+major = 1
+minor = 0
+patch = -1  # 1dca4be7d0990856ab43114883c3729b0c901e47
