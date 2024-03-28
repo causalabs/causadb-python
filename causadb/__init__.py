15af7ec7afa70b77b35bbf6c5fb445386aeb4b6c
JHart96 <jordan.da.hart@gmail.com>
2024-03-28 11:59:33 +0000

    Update __init__.py

--- causadb/__init__.py
+++ causadb/__init__.py
@@ -1,5 +1,5 @@
-from .causadb import *
-from .model import *
-from .data import *
+from .causadb import CausaDB
+from .model import Model
+from .data import Data
 
 from .__version__ import __version__
